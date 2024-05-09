ec54af70995b8ef66b61548e4b52879cff6d261a
rinkwork <rpwork106@gmail.com>
2024-05-09 12:27:18 +0530

    udemy course

---
+++ 3_meta_data.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('images/cat.jpg')
+height, width, depth = np.shape(img)
+
+print(f'Height = {height}')
+print(f'width = {width}')
+print(f'depth = {depth}')
+
+cv2.imshow('test image', img)
+cv2.waitKeyEx(0)
+cv2.destroyAllWindows()
